e416dbc4dfccc06544572725b300174b9d6ee0d8
Mohammad <fadaeian.mohammad@gmail.com>
2022-10-28 18:33:24 +0330

    change userservice

--- PasswordManager.Service/Services/UserService.cs
+++ PasswordManager.Service/Services/UserService.cs
@@ -1,4 +1,7 @@
-﻿using PasswordManager.Repository.Interfaces;
+﻿using AutoMapper;
+using PasswordManager.Entity.Models;
+using PasswordManager.Repository.Interfaces;
+using PasswordManager.Repository.Repositories;
 using PasswordManager.Service.DTOs;
 using PasswordManager.Service.Interfaces;
 using System;
@@ -6,22 +9,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace PasswordManager.Service.Services
 {
-    public class UserService:IUserService
+    public class UserService : IUserService
     {
         private readonly IUserRepository _userRepository;
-        public UserService(IUserRepository userRepository)
+        private readonly IMapper _mapper;
+        public UserService(IMapper mapper,IUserRepository userRepository)
         {
             _userRepository = userRepository;
+            _mapper = mapper;
         }
 
-        public List<UserListItemDTO> GetAllUsers() {
+        public List<UserListItemDTO> GetAllUsers()
+        {
 
-          List<UserListItemDTO> ouput = new List<UserListItemDTO>();
+            List<UserListItemDTO> output = new List<UserListItemDTO>();
 
-          return ouput;
+            output = _mapper.Map<List<UserListItemDTO>>(_userRepository.ReadAll());
+            
+            return output;
 
         }
     }
