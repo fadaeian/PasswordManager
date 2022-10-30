340ca2d76265392d64ed305535ccbf64f12f85fb
Mohammad <fadaeian.mohammad@gmail.com>
2022-10-30 08:33:33 +0330

    Add ApiResutDTO

---
+++ PasswordManager.DTO/ApiResultDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordManager.DTO
+{
+    public class ApiResultDTO<T>
+    {
+        public T Data { get; set; }
+        public bool Success { get; set; }
+        public string Error { get; set; }
+    }
+}
---
+++ PasswordManager.DTO/UserListItemDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.DTO
+{
+    public class UserListItemDTO
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Locked { get; set; }
+    }
+}
