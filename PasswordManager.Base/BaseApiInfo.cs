af84b216bb9bdd3067708fd32f71697faa3bac7a
Mohammad <fadaeian.mohammad@gmail.com>
2022-10-30 08:36:55 +0330

    solution changes

---
+++ PasswordManager.Base/BaseApiInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Base
+{
+	public static class BaseApiInfo
+	{
+		public static string ApiAddress { get; set; } = string.Empty;
+		public static string AuthKey { get; set; } = string.Empty;
+	}
+}
